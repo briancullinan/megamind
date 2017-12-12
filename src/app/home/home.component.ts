a5442f6bbc91e4cb366b6db7b180645ced76333a
SWIFTPAGE\Brian.Cullinan <megamindbrian@gmail.com>
2017-12-12 14:58:58 -0700

    no message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import * as Fuse from 'fuse.js';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/startWith';
+import { RpcService } from './rpc-service';
+import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/observable/zip';
-import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'bc-home',
     templateUrl: './home.component.html',
-    styleUrls: [ './home.component.scss' ]
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
     search = 'tell joke';
@@ -21,138 +16,33 @@ export class HomeComponent implements OnInit, OnDestroy {
     functionName = '';
     functionParameters: Array<string> = [];
     parameters: { [index: string]: any } = {};
-
-    permissions: { [index: string]: any } = {};
-    permissionsFiltered: Observable<Array<string>>;
-    permissionsAccessible: Observable<Array<string>>;
-    permissionsLocked: Observable<Array<string>>;
-    permissionsFunctions: Observable<Array<string>>;
-
     myControl = new FormControl();
-    private fuse: Fuse;
-    private searchObservable: Observable<Array<string>>;
-    private error: boolean;
+    error: boolean;
 
-    constructor(public http: HttpClient,
+    private searchSub: Subscription;
+    private parameterSub: Subscription;
+
+    constructor(public rpc: RpcService,
                 public ref: ChangeDetectorRef) {
     }
 
     ngOnInit(): void {
-        this.getParameters();
-        this.getPermissions();
+        this.setupParameters();
+        this.searchSub = this.myControl.valueChanges
+            .debounceTime(100)
+            .subscribe(s => this.rpc.search(s));
     }
 
     ngOnDestroy(): void {
-    }
-
-    getPermissions() {
-        this.http.get(environment.rpcAddress + '/getPermissions')
-            .subscribe((r: { [index: string]: any }) => {
-                this.permissions = r;
-                this.filterPermissions();
-            }, e => {
-            });
-    }
-
-    filterPermissions() {
-        const FUSE_CONFIG = {
-            caseSensitive: false,
-            findAllMatches: true,
-            distance: 100,
-            threshold: 0.5,
-            tokenize: false,
-            shouldSort: true,
-            keys: [ 'question' ],
-            id: 'id'
-        };
-        const database = Object.keys(this.permissions).map(k => ({
-            id: k,
-            question: k.split(/[\[\]]/ig)[ 1 ],
-            permissions: this.permissions[ k ]
-        }));
-        this.fuse = new Fuse(database, FUSE_CONFIG);
-        this.searchObservable = this.myControl.valueChanges
-            .startWith(this.search)
-            .map(name => {
-                this.search = name;
-                return name && name.length > 0
-                    ? this.fuse.search<string>(name)
-                    : Object.keys(this.permissions);
-            });
-
-        this.permissionsFiltered = this.searchObservable
-            .map((arr: Array<string>) => arr
-                .map(r => r.split(/[\[\]]/ig)[ 1 ]));
-
-        this.permissionsAccessible = this.searchObservable
-            .map((arr: Array<string>) => arr
-                .filter(r => this.permissions[ r ].indexOf('Public') > -1)
-                .map(r => r.split(/[\[\]]/ig)[ 1 ])
-                .slice(0, 10));
-
-        this.permissionsLocked = this.searchObservable
-            .map((arr: Array<string>) => arr
-                .filter(r => this.permissions[ r ].indexOf('Public') === -1
-                    && this.permissions[ r ].indexOf('Function') === -1)
-                .map(r => r.split(/[\[\]]/ig)[ 1 ])
-                .slice(0, 10));
-
-        this.permissionsFunctions = this.searchObservable
-            .map((arr: Array<string>) => arr
-                .filter(r => this.permissions[ r ].indexOf('Public') === -1
-                    && this.permissions[ r ].indexOf('Function') > -1)
-                .map(r => r.split(/[\[\]]/ig)[ 1 ])
-                .slice(0, 10));
-
-        this.permissionsFiltered
-            .debounceTime(500)
-            .subscribe(r => this.getParameters());
-        this.ref.detectChanges();
-    }
-
-    getParameters() {
-        if (typeof this.fuse === 'undefined') {
-            return;
+        if (typeof this.searchSub !== 'undefined') {
+            this.searchSub.unsubscribe();
         }
-        const search = this.fuse.search<string>(this.search);
-        console.log(search);
-        if (search.length === 0 || typeof this.permissions[ search[ 0 ] ] === 'undefined'
-            || this.permissions[ search[ 0 ] ].indexOf('Function') === -1) {
-            this.error = true;
-            this.ref.detectChanges();
-            return;
+        if (typeof this.parameterSub !== 'undefined') {
+            this.parameterSub.unsubscribe();
         }
-        this.error = false;
-        this.loading = true;
-        const keep = this.functionParameters.map(p => this.parameters[ p ]);
-        this.functionName = '';
-        this.functionParameters = [];
-        this.parameters = {};
-        if (this.history.length > 5) {
-            this.history.splice(5);
-        }
-        this.http.get(environment.rpcAddress + '/getParameterNames?search=' + this.search)
-            .subscribe((r: Array<string>) => {
-                this.loading = false;
-                this.functionName = r[ 0 ];
-                this.functionParameters = r.slice(1);
-                this.parameters = this.functionParameters.reduce((acc, p, i) => {
-                    acc[ p ] = keep[ i ];
-                    return acc;
-                }, {} as { [index: string]: any });
-                this.ref.detectChanges();
-            }, e => {
-                this.loading = false;
-                this.functionName = '';
-                this.functionParameters = [];
-                this.parameters = {};
-                this.history.unshift([ e, JSON.stringify(e, null, 4) ]);
-                this.ref.detectChanges();
-            });
-        this.ref.detectChanges();
     }
 
-    sendCommand() {
+    sendCommand(): boolean {
         this.loading = true;
         if (typeof this.functionName === 'undefined'
             || this.functionName === '') {
@@ -161,25 +51,57 @@ export class HomeComponent implements OnInit, OnDestroy {
             // TODO: call import with the search terms if it is not a function
         }
 
-        this.history.unshift([ 'megamind', this.search, JSON.stringify(this.parameters, null, 4) ]);
+        this.history.unshift(['megamind', this.functionName, JSON.stringify(this.parameters, void 0, 4)]);
         if (this.history.length > 5) {
             this.history.splice(5);
         }
 
-        console.log(environment.rpcAddress + '/' + this.functionName);
-        this.http.post(
-            environment.rpcAddress + '/' + this.functionName,
-            this.parameters)
-            .subscribe(r => {
+        this.rpc.call(this.functionName, this.parameters)
+            .subscribe((r: any) => {
                 this.loading = false;
-                this.history.unshift([ r, JSON.stringify(r, null, 4) ]);
+                this.history.unshift([r, JSON.stringify(r, void 0, 4)]);
                 this.ref.detectChanges();
-            }, e => {
+            }, (e: any) => {
+                console.log(e);
                 this.loading = false;
-                this.history.unshift([ e, JSON.stringify(e, null, 4) ]);
+                this.history.unshift([e, JSON.stringify(e, void 0, 4)]);
                 this.ref.detectChanges();
             });
         this.ref.detectChanges();
         return false;
+    }
+
+    private setupParameters(): void {
+        this.parameterSub = this.rpc.permissionsFiltered
+            .flatMap(s => {
+                this.error = false;
+                if (this.history.length > 5) {
+                    this.history.splice(5);
+                }
+                this.functionName = s[0];
+                console.log(s.length);
+                if (this.search.length === 0) {
+                    return Observable.of([]);
+                }
+                this.ref.detectChanges();
+                return this.rpc.parameters(s[0]);
+            })
+            .subscribe((r: Array<string>) => {
+                const keep = this.functionParameters.map(p => this.parameters[p]);
+                this.loading = false;
+                this.functionParameters = r.slice(1);
+                this.parameters = this.functionParameters.reduce((acc, p, i) => {
+                    acc[p] = keep[i];
+                    return acc;
+                }, {} as { [index: string]: any });
+                this.ref.detectChanges();
+            }, e => {
+                console.log(e);
+                this.error = true;
+                this.loading = true;
+                this.functionParameters = [];
+                this.parameters = {};
+                this.ref.detectChanges();
+            });
     }
 }
--- src/app/home/rpc-service.ts
+++ src/app/home/rpc-service.ts
@@ -1,10 +1,139 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import * as Fuse from 'fuse.js';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/observable/throw';
+import { Subscription } from 'rxjs/Subscription';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class RpcService {
+    lastResult: any;
+    permissionsSub: Subscription;
+    searchSub: Subscription;
+
+    permissionsFiltered: Observable<Array<string>>;
+    permissionsAccessible: Observable<Array<string>>;
+    permissionsLocked: Observable<Array<string>>;
+    permissionsFunctions: Observable<Array<string>>;
+
+    private fuse: Fuse;
+    private inputSearch = new BehaviorSubject<string>('');
+    private searchObservable: Observable<Array<string>>;
+    private permissionsObservable = new ReplaySubject<{ [index: string]: any }>(1);
 
     constructor(public http: HttpClient) {
+        this.setupPermissions();
+        this.setupFilters();
+    }
+
+    public call(search: string, body: { [index: string]: any } = void 0, isPost = false): Observable<object> {
+        return isPost
+            ? this.http.post(environment.rpcAddress + '/rpc?function=' + search, body)
+            : this.http.get(environment.rpcAddress + '/rpc?function=' + search, {params: body});
+    }
+
+    public search(query: string): void {
+        this.inputSearch.next(query);
+    }
+
+    public parameters(search: string): Observable<Array<string>> {
+        return this.permissionsObservable
+            .flatMap(permissions => {
+                const results = this.fuse.search<string>(search);
+                if (results.length === 0 || typeof permissions[results[0]] === 'undefined'
+                    || permissions[results[0]].indexOf('Function') === -1) {
+                    return Observable.of([]);
+                }
+                return this.call('get parameter names', {search});
+            })
+            .map((r: Array<string>) => {
+                return r;
+            });
+    }
+
+    private setupSearch(permissions: { [index: string]: any }): void {
+        const FUSE_CONFIG = {
+            caseSensitive: false,
+            findAllMatches: true,
+            distance: 100,
+            threshold: 0.5,
+            tokenize: false,
+            shouldSort: true,
+            keys: ['question'],
+            id: 'id'
+        };
+        const database = Object.keys(permissions).map(k => ({
+            id: k,
+            question: k.split(/[\[\]]/ig)[1],
+            permissions: permissions[k]
+        }));
+        this.fuse = new Fuse(database, FUSE_CONFIG);
+    }
+
+    private setupPermissions(): void {
+        this.permissionsSub = this.call('get permissions')
+            .subscribe((permissions: { [index: string]: any }) => {
+                this.setupSearch(permissions);
+                this.permissionsObservable.next(permissions);
+            });
+    }
+
+    private setupFilters(): void {
+        this.searchObservable = Observable
+            .combineLatest(
+                this.inputSearch,
+                this.permissionsObservable,
+                (name: string, permissions: { [index: string]: any }) => ({name, permissions}))
+            .map(({name, permissions}) => {
+                console.log('searching ' + name);
+                return name && name.length > 0
+                    ? this.fuse.search<string>(name)
+                    : Object.keys(permissions);
+            });
+
+        this.permissionsFiltered = this.searchObservable
+            .map((arr: Array<string>) => arr
+                .map(r => r.split(/[\[\]]/ig)[1]));
+
+        this.permissionsAccessible = Observable
+            .combineLatest(
+                this.searchObservable,
+                this.permissionsObservable,
+                (arr: Array<string>, permissions) => ({arr, permissions}))
+            .map(({arr, permissions}) => arr
+                .filter(r => permissions[r].indexOf('Public') > -1)
+                .map(r => r.split(/[\[\]]/ig)[1])
+                .slice(0, 10));
+
+        this.permissionsLocked = Observable
+            .combineLatest(
+                this.searchObservable,
+                this.permissionsObservable,
+                (arr: Array<string>, permissions) => ({arr, permissions}))
+            .map(({arr, permissions}) => arr
+                .filter(r => permissions[r].indexOf('Public') === -1
+                    && permissions[r].indexOf('Function') === -1)
+                .map(r => r.split(/[\[\]]/ig)[1])
+                .slice(0, 10));
+
+        this.permissionsFunctions = Observable
+            .combineLatest(
+                this.searchObservable,
+                this.permissionsObservable,
+                (arr: Array<string>, permissions) => ({arr, permissions}))
+            .map(({arr, permissions}) => arr
+                .filter(r => permissions[r].indexOf('Public') === -1
+                    && permissions[r].indexOf('Function') > -1)
+                .map(r => r.split(/[\[\]]/ig)[1])
+                .slice(0, 10));
     }
 
 }
